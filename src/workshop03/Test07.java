3539cd441896621dd7548641169c1836dd85d5f4
Kim Bohee <boingkimm@gmail.com>
2023-07-17 18:21:29 +0900

    Scanner

---
+++ src/workshop03/Test07.java
@@ -0,0 +1,17 @@
+package workshop03;
+
+import java.util.Scanner;
+
+public class Test07 {  //TODO 문제풀기
+
+	public static void main(String[] args) {
+	/*
+	 * 1부터 100까지 더하는 프로그램을 작성 한다.
+	 * 단, Scanner로 1자리 정수형 데이터를 입력 받아 입력 받은 수의 배수만 더한다.
+	 * 입력 예1: 5 
+			5,10,15, … , 100의 합
+	 */
+		
+	}
+
+}
