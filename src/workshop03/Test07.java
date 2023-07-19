2a31357240d4659c1d76faaf92de22df42e04d56
Kim Bohee <boingkimm@gmail.com>
2023-07-19 13:55:53 +0900

    Scanner

--- src/workshop03/Test07.java
+++ src/workshop03/Test07.java
@@ -2,7 +2,7 @@ package workshop03;
 
 import java.util.Scanner;
 
-public class Test07 {  //TODO 문제풀기
+public class Test07 {
 
 	public static void main(String[] args) {
 	/*
@@ -12,6 +12,19 @@ public class Test07 {  //TODO 문제풀기
 			5,10,15, … , 100의 합
 	 */
 		
-	}
-
+		//TODO
+		Scanner scan = new Scanner(System.in);
+		System.out.println("한 자리 정수값을 입력하시오 ");
+		int numberValue = scan.nextInt();
+		int resultValue = 0;
+		for (int inx = 1; inx <= 100; inx++) {
+			if ((inx % numberValue) == 0) {
+				resultValue += inx;
+				if(inx!=numberValue)System.out.print("+");
+				System.out.print(inx);
+				
+			}
+		}//end for
+		System.out.print(" = " + resultValue);
+	}//end main
 }
